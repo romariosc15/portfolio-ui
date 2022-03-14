461a8780f04093e39f0830e1c4e834f4cb98bafa
romariosc97 <romarito_97@hotmail.com>
2022-03-14 02:44:01 -0500

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,19 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+    head: {
+        title: 'Romario Sarmiento',
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          {
+            hid: 'description hid',
+            name: 'description name',
+            content: 'my website description'
+          }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
     buildModules: [
         
     ],
