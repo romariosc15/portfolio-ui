0ac642caa0a0bf0e6ab346bfea3076cf8be23e61
romariosc97 <romarito_97@hotmail.com>
2022-06-11 00:50:03 -0500

    Responsive improvement

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
-    <div id="contact" class="h-screen md:grid md:grid-cols-5 lg:grid-cols-10 2xl:grid-cols-7 lg:gap-2 2xl:gap-4">
-        <div class="mx-6 md:mx-12 lg:mr-0 lg:ml-12 2xl:ml-20 pt-8 md:pt-12 2xl:pt-24 md:col-span-3 lg:col-span-7 2xl:col-span-4">
+    <div id="contact" class="lg:h-screen md:grid md:grid-cols-5 lg:grid-cols-10 2xl:grid-cols-7 lg:gap-2 2xl:gap-4">
+        <div class="mx-6 md:mx-12 lg:mr-0 lg:ml-12 2xl:ml-20 pb-12 lg:pb-0 pt-8 md:pt-12 2xl:pt-24 md:col-span-3 lg:col-span-7 2xl:col-span-4">
             <h1 class="text-3xl md:text-5xl xl:text-6xl 2xl:text-7xl font-extrabold">Contacto</h1>
             <p class="mt-4 md:mt-6 2xl:mt-8 text-md md:text-lg lg:text-lg 2xl:text-xl font-light text-justify text-gray-500">¿Tienes en mente algún proyecto? No dudes en ponerte en contacto conmigo.</p>
             <div class="mt-6 2xl:mt-8 grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
@@ -18,7 +18,7 @@
                 </div>
             </div>
             <p class="mt-6 2xl:mt-8 text-md md:text-lg lg:text-lg 2xl:text-xl font-light text-justify text-gray-500">Encuentrame también por redes sociales.</p>
-            <div class="mt-4 xl:mt-6 2xl:mt-8 space-y-4 xl:space-y-6 mb-6 lg:mb-0">
+            <div class="mt-4 xl:mt-6 2xl:mt-8 space-y-4 xl:space-y-6">
                 <div>
                     <img class="w-6 h-6 md:w-7 lg:w-8 md:h-7 lg:h-8 inline-block" src="~/assets/img/social-medias/linkedin.png" />
                     <a target="_blank" href="https://www.linkedin.com/in/romariosarmiento/" class="text-sm md:text-sm lg:text-md inline-block ml-4 font-light hover:underline">Jaime Romario Sarmiento</a>
--- components/Home.vue
+++ components/Home.vue
@@ -2,8 +2,8 @@
     <div id="home" class="flex items-center justify-center h-screen flex-col bg-gray-50">
         <img class="w-44 h-44 md:w-64 md:h-64 2xl:w-72 2xl:h-72 rounded-full object-cover border-4 md:border-8 border-gray-300" src="~/assets/img/home/main.jpg" />
         <h1 class="mt-6 2xl:mt-8 text-3xl md:text-5xl lg:text-6xl 2xl:text-7xl font-extrabold">Romario Sarmiento</h1>
-        <p class="mt-4 md:mt-6 mx-5 text-center text-xs md:text-md lg:text-md 2xl:text-lg text-gray-500">Desarrollador web con más de 3 años de experiencia trabajando con equipos angloparlantes.</p>
-        <p class="mt-1 sm:mt-0 mx-5 text-center text-xs md:text-md lg:text-md 2xl:text-lg text-gray-500">Con capacidad para desarrollar software mantenible, funcional, seguro y documentado.</p>
+        <p class="mt-4 md:mt-6 mx-5 text-center text-sm md:text-base lg:text-lg 2xl:text-xl text-gray-500">Desarrollador web con más de 3 años de experiencia trabajando con equipos angloparlantes.</p>
+        <p class="mt-1 sm:mt-0 mx-5 text-center text-sm md:text-base lg:text-lg 2xl:text-xl text-gray-500">Con capacidad para desarrollar software mantenible, funcional, seguro y documentado.</p>
         <div class="mt-6 2xl:mt-8 space-x-8">
             <a target="_blank" class="inline-block" href="https://github.com/romariosc15">
                 <img class="w-6 h-6 md:w-8 md:h-8" src="~/assets/img/social-medias/github.png" />
