5be05b7bcd18f668010d47d6928d31edd1b39ed5
romariosc97 <romarito_97@hotmail.com>
2022-06-14 01:16:23 -0500

    Img path fixed

--- components/Timeline.vue
+++ components/Timeline.vue
@@ -2,7 +2,7 @@
     <ol class="">
         <li v-for="(value, index) in experience" v-bind:key="value.company">
             <div class="flex flex-row">
-                <img class="w-8 md:w-10 lg:w-10 2xl:w-12 h-8 md:h-10 lg:h-10 2xl:h-12 object-cover rounded-full mb-2 lg:mb-2 2xl:mb-3 bg-gray-50" :src="`assets/img/experience/${value.image}`" alt="">
+                <img class="w-8 md:w-10 lg:w-10 2xl:w-12 h-8 md:h-10 lg:h-10 2xl:h-12 object-cover rounded-full mb-2 lg:mb-2 2xl:mb-3 bg-gray-50" :src="`/_nuxt/assets/img/experience/${value.image}`" alt="">
                 <div class="ml-3.5 md:ml-3.5 lg:ml-3.5 2xl:ml-4">
                     <p class="text-xs lg:text-sm 2xl:text-lg font-bold">{{value.company}}</p>
                     <p class="text-xs lg:text-sm 2xl:text-base text-gray-500 font-light"><span class="font-normal">{{value.position}}</span> ({{value.period}})</p>
